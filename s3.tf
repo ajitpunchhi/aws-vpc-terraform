f598eb6e6817dc91cd63ddf3c9717f2df483eb9b
ajitpunchhi <ajit.1431@gmail.com>
2025-07-01 18:43:15 +0530

    new updated file

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "meewbucket" {
+resource "aws_s3_bucket" "updatedbucket" {
   bucket = "my-sandbox-bucket-${var.aws_region}"
   tags = {
     Name        = "My Sandbox Bucket"
