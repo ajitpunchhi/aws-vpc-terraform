0131ef263719bf84cf8759470116424fb7ed4d40
ajitpunchhi <ajit.1431@gmail.com>
2025-07-05 11:40:34 +0530

    s3 updated

--- s3.tf
+++ s3.tf
@@ -1,18 +1,18 @@
-resource "aws_s3_bucket" "updatedbucket" {
-  bucket = "my-sandbox-bucket-${var.aws_region}"
-  tags = {
-    Name        = "My Sandbox Bucket"
-    Environment = "Sandbox"
-    CreatedBy   = "Terraform"
-    Project     = "Infrastructure"
-    Owner       = "DevOps Team"
-  }
-}
+# resource "aws_s3_bucket" "updatedbucket" {
+#   bucket = "my-sandbox-bucket-${var.aws_region}"
+#   tags = {
+#     Name        = "My Sandbox Bucket"
+#     Environment = "Sandbox"
+#     CreatedBy   = "Terraform"
+#     Project     = "Infrastructure"
+#     Owner       = "DevOps Team"
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "name" {
+# resource "aws_s3_bucket_versioning" "name" {
   
-  bucket = aws_s3_bucket.updatedbucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   bucket = aws_s3_bucket.updatedbucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
